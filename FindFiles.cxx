08f1ca8c520ea0361ed0e45af2852d8846a28b3f
dcoplowe <david.coplowe@physics.ox.ac.uk>
2017-03-09 14:04:10 +0000

    First commit

--- FindFiles.cxx
+++ FindFiles.cxx
@@ -48,7 +48,7 @@ FindFiles::FindFiles(std::string infilename, std::string oa_list){
     string outdir = infilename;
     if( (point = outdir.find(m_outfilename.c_str()) ) != std::string::npos){
         cout << "Point = " << point << endl;
-        outdir.substr(0,point);
+        outdir = outdir.substr(0,point);
     }
     cout << "outdir = " << outdir << endl;
     cout << "m_outfilename = " << m_outfilename << endl;
