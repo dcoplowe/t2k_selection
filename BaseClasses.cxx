cbaaa58bd68d078480838a58f476baeffa6e35d7
dcoplowe <david.coplowe@physics.ox.ac.uk>
2017-03-11 18:31:01 +0000

    First commit

--- BaseClasses.cxx
+++ BaseClasses.cxx
@@ -1,5 +1,9 @@
 #include "BaseClasses.h"
 
+namespace ND {
+    class TTrueVertex;
+} // end of namespace.
+
 #ifndef ND__TTrueVertex_cxx
 #define ND__TTrueVertex_cxx
 ND::TTrueVertex::TTrueVertex() {
