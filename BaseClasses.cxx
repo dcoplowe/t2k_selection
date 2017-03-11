20b1da26fd1b2009d65d1968349e0c97fae2ffd9
dcoplowe <david.coplowe@physics.ox.ac.uk>
2017-03-11 18:16:31 +0000

    First commit

--- BaseClasses.cxx
+++ BaseClasses.cxx
@@ -1,8 +1,10 @@
+
+
 #ifndef ND__TTrueVertex_cxx
 #define ND__TTrueVertex_cxx
-TTrueVertex::TTrueVertex() {
+ND::TTrueVertex::TTrueVertex() {
 }
-TTrueVertex::TTrueVertex(const TTrueVertex & rhs)
+ND::TTrueVertex::TTrueVertex(const TTrueVertex & rhs)
 : TObject(const_cast<TTrueVertex &>( rhs ))
 , Position(const_cast<TTrueVertex &>( rhs ).Position)
 , ID(const_cast<TTrueVertex &>( rhs ).ID)
@@ -13,25 +15,6 @@ TTrueVertex::TTrueVertex(const TTrueVertex & rhs)
     // Use at your own risk!
     if (&rhs) {} // avoid warning about unused parameter
 }
-TTrueVertex::~TTrueVertex() {
+ND::TTrueVertex::~TTrueVertex() {
 }
 #endif // ND__TTrueVertex_cxx
-
-#ifndef ND__TTrueParticle_cxx
-#define ND__TTrueParticle_cxx
-TTrueParticle::TTrueParticle() {
-}
-TTrueParticle::TTrueParticle(const TTrueParticle & rhs)
-: TObject(const_cast<TTrueParticle &>( rhs ))
-, ID(const_cast<TTrueParticle &>( rhs ).ID)
-, Pur(const_cast<TTrueParticle &>( rhs ).Pur)
-, Eff(const_cast<TTrueParticle &>( rhs ).Eff)
-, Vertex(const_cast<TTrueParticle &>( rhs ).Vertex)
-{
-    // This is NOT a copy constructor. This is actually a move constructor (for stl container's sake).
-    // Use at your own risk!
-    if (&rhs) {} // avoid warning about unused parameter
-}
-TTrueParticle::~TTrueParticle() {
-}
-#endif // ND__TTrueParticle_cxx
